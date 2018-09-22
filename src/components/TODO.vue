7493399a90e303f87bfa8d3f6964cf0626ace842
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-22 18:42:19 +0800

    todo

---
+++ src/components/TODO.vue
@@ -0,0 +1,19 @@
+<template>
+<el-input
+        type="textarea"
+        :rows="2"
+        placeholder="请输入内容"
+        v-model="textarea">
+</el-input>
+</template>
+<script>
+    export default {
+        name:'todo'
+    }
+</script>
+
+<style scoped lang="scss">
+ e-input{
+     width:50%;
+ }
+</style>
---
+++ src/components/Things.vue
@@ -0,0 +1,42 @@
+<template>
+    <el-table
+            :data="tableData"
+
+            max-height="250">
+        <el-table-column
+                prop="todo"
+                label="要做的事">
+        </el-table-column>
+        <el-table-column
+                fixed="right"
+                label="操作"
+                width="120">
+            <template slot-scope="scope">
+                <el-button
+                        @click.native.prevent="deleteRow(scope.$index, tableData)"
+                        type="text"
+                        size="small">
+                    移除
+                </el-button>
+            </template>
+        </el-table-column>
+    </el-table>
+</template>
+
+<script>
+    export default {
+        methods: {
+            deleteRow(index, rows) {
+                rows.splice(index, 1);
+            }
+        },
+        data() {
+            return {
+                tableData: [{todo:123},{todo:123},{todo:123},{todo:123},]
+            }
+        }
+    }
+</script>
+<style scoped>
+
+</style>
