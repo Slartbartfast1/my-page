df85e6c58db56c3a2400d5a3157cbd2a379ba0dd
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-24 00:23:43 +0800

    头部

---
+++ src/components/header1.vue
@@ -0,0 +1,81 @@
+<template>
+    <nav>
+        <div class="logo">
+            <i class="el-icon-question"></i>
+            <span>儿乎?</span>
+        </div>
+        <div class="user">
+            <a href="#">登陆</a>/
+            <a href="#">注册</a>
+        </div>
+        <div class="search">
+        <el-input v-model="input" placeholder="请输入内容">
+        </el-input>
+            <i class="el-icon-search"></i>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "header1"
+    }
+</script>
+
+<style scoped lang="scss">
+nav{
+    height:60px;
+    width:100%;
+    background-color: darkcyan;
+    position:fixed;
+    top:0;
+    .logo{
+        float:left;
+        height:100%;
+        i{
+            cursor: pointer;
+            margin-left: 20px;
+            font-size:50px;
+            float: left;
+            color: white;
+            line-height:60px;
+        }
+        span{
+            cursor: pointer;
+            float: left;
+            font-size:30px;
+            color: white;
+            line-height:60px;
+            padding-left: 10px;
+        }
+    }
+    .search{
+        padding:10px 0 ;
+        position: relative;
+        float: right;
+        width:200px;
+        height: 60px;
+        margin-right:10px;
+        i{
+            position:absolute;
+            right:4px;
+            top:21px;
+            cursor:pointer;
+        }
+    }
+    .user{
+        line-height:60px;
+        float: right;
+        margin-right:30px;
+        color:white;
+        a{
+            text-decoration:none;
+            color:white;
+        }
+        a:hover{
+            text-decoration:underline;
+        }
+    }
+}
+
+</style>
