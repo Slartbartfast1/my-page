d22dba7001f710ff2bfaf8fb151864c2a04eebad
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-24 16:54:58 +0800

    登陆

---
+++ src/views/login.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+        <div class="hero">
+        </div>
+        <div class="login">
+            <span>登陆</span>
+            <router-link to="/register">
+                注册
+            </router-link>
+            <el-form label-position="left" label-width="80px" :model="formLabelAlign">
+                <el-form-item label="用户名:">
+                    <el-input></el-input>
+                </el-form-item>
+                <el-form-item label="密码:">
+                    <el-input type="password"></el-input>
+                </el-form-item>
+                <el-button type="primary">登陆</el-button>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        name: "login",
+        data() {
+            return {
+                labelPosition: '',
+                formLabelAlign: {
+                    name: '',
+                    region: '',
+                    type: ''
+                }
+            };
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+
+    .hero {
+        background-color: #30C8CD;
+        width: 100%;
+        height: 350px;
+
+    }
+    span{
+        position: relative;
+        margin:0 auto;
+        font-size:50px;
+        line-height:100px;
+    }
+    .login {
+        padding: 20px;
+        position: relative;
+        border-radius: 3px;
+        width: 400px;
+        margin: -160px auto 0 auto;
+        background-color: #FFF;
+        box-shadow: 0 0 10px rgba(0, 0, 0, .1);
+        transition: .3s all ease;
+    }
+
+    .login:hover {
+        box-shadow: 0 0 10px rgba(0, 0, 0, .3);
+    }
+
+    button {
+        width: 100%;
+    }
+</style>
