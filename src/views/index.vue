a78762542669dbb3b9454627863f2ce07a2abd45
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-24 16:54:54 +0800

    主页

---
+++ src/views/index.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+        <header1></header1>
+        <el-row :gutter="20">
+            <el-col :span="18">
+                <TODO></TODO>
+                <questions></questions>
+            </el-col>
+            <el-col :span="6"><Things></Things></el-col>
+        </el-row>
+</div>
+</template>
+<script>
+    import TODO from '../components/TODO.vue'
+    import Things from '../components/Things.vue'
+    import header1 from '../components/header1.vue'
+    import questions from '../components/questions.vue'
+
+    export default {
+        name: 'index',
+        components: {
+            TODO, Things, header1,questions
+        },
+    }
+</script>
+
+<style>
+
+</style>
