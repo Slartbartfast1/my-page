971267ac444b91285ce203321c6fcfe3890214ea
Slartbartfast1 <38152942+Slartbartfast1@users.noreply.github.com>
2018-09-24 16:55:22 +0800

    主体

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,26 @@
 <template>
-  <div id="app">
-    <div class="nav">
-      <router-link to="/about">emails</router-link>
-
-      <router-link to="/">home</router-link>
+    <div id="app">
+        <router-view>
+        </router-view>
     </div>
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-
-        below
-      </p>
-
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+    }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+
+    }
+
+    body, html {
+        margin: 0;
+        padding: 0;
+    }
 </style>
